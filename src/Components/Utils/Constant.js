717ef674c328ddf6a6777ae1af6a0f9fdb586b32
satya960 <satya9604166@gmail.com>
2024-01-29 14:48:19 +0530

    first commit

--- src/Components/Utils/Constant.js
+++ src/Components/Utils/Constant.js
@@ -2,4 +2,7 @@ const Google_Api_key ="AIzaSyArvcOkXjCvuKUB7FTa5Tk4A_X3Gtz5bfM";
 
 export  const YOUTUBE_VIDEO_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+Google_Api_key ;
 
-export const YOUTUBE_SEARCH_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=" ;
+export const YOUTUBE_SEARCH_API = "https://clients1.google.com/complete/search?client="
+
+//"http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=" ;
+
